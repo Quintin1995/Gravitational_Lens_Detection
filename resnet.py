6cf1c539661660bb4fd3e9d9df14cdc74a0273d1
quintin <qvlohuizen@live.nl>
2020-06-19 12:26:21 +0200

    i forgot to link the network

--- resnet.py
+++ resnet.py
@@ -234,7 +234,7 @@ class ResnetBuilder(object):
         # Classifier block
         block_shape = K.int_shape(block)
         pool2 = AveragePooling2D(pool_size=(block_shape[ROW_AXIS], block_shape[COL_AXIS]), strides=(1, 1))(block)
-        flatten1 = Flatten()(block)
+        flatten1 = Flatten()(pool2)
         dense = Dense(units=num_outputs, kernel_initializer="he_normal", activation="sigmoid")(flatten1)
 
         model = Model(inputs=input, outputs=dense)
